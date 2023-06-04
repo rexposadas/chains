ac6b88e73d30cf3343eb4b20c49eaca3ac5964d6
Rex Posadas <rexposadas@gmail.com>
2023-06-04 22:03:36 +0800

    added test for hashing

--- models/block_util_test.go
+++ models/block_util_test.go
@@ -1,8 +1,27 @@
 package models
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 // TODO: Tirso -> test all the functions in block_util.go file
 func Test_GenerateBlock(t *testing.T) {
 
+}
+
+func Test_CalculateHash(t *testing.T) {
+
+	testCases := []struct {
+		B Block
+	}{
+		{B: Block{Bpm: 34}},
+		{B: Block{}},
+	}
+
+	for _, item := range testCases {
+		result := CalculateHash(item.B)
+		assert.NotEmpty(t, result)
+		t.Log(result)
+	}
 }
